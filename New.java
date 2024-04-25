5e0625e806befa3ef659328dec73e3afb077a924
aditya <adityarai2312@gmail.com>
2024-04-25 14:30:47 +0530

    First Commit

---
+++ New.java
@@ -0,0 +1,10 @@
+class New
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Aditya Rai");
+		System.out.println("Nidhi");
+		System.out.println("Domar");
+		System.out.println("Sonu");
+	}
+}
